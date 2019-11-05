8dc274b4344e3761951de2bf2d2b275bb66fedec
Luis Javier <luis-javier.hernandez@unirioja.es>
2019-11-05 12:34:02 +0100

    Some examples (con :tk y gcf())

---
+++ Examples/3rootsNewton.jl
@@ -0,0 +1,16 @@
+
+import Pkg
+
+Pkg.status()
+using PyCall
+pygui(:tk)
+using PyPlot
+
+import PBURF
+
+coefficientlistnumcompila=complex([1.,0.,0.,2.])
+coefficientlistdencompila=complex([0.,0.,3.,0.])
+result=PBURF.plottingBasinsUnivariateRationalFunctions(coefficientlistnumcompila,
+    coefficientlistdencompila)
+
+gcf()
